e5f8cbe8caa91f25ab0444aa0e923e9b04cf2656
natafajado <nataliafajardor1207@gmail.com>
2022-01-18 10:40:34 +0100

    configuracion router

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,24 @@
 import logo from './logo.svg';
 import './App.css';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
 // import { AccessAlarm, ThreeDRotation } from '@mui/icons-material';
 import Login from './components/auth/Login'
+import Aliado from './components/pages/Aliado'
+import Administrador from './components/pages/Administrador'
+
 
 function App() {
   return (
-    <Login></Login>
-  );
+    
+    <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Login />} />
+          <Route path="Aliado" element={<Aliado />} />
+          <Route path="Administrador" element={<Administrador/>} />
+        </Routes>
+      </BrowserRouter>
+  )
 }
 
 export default App;
+
---
+++ src/components/pages/Aliado.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Aliado = () => {
+  return (
+    <div>
+      <h1>aliado</h1>
+    </div>
+  )
+}
+
+export default Aliado
