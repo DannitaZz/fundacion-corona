f24d11dd0b57a431ed9057a6e5fd7a4c66e525a8
TefyRabih <Tefyrabih@gmail.com>
2022-01-18 17:13:34 -0500

    Inicio componente Metodologia Oso

--- src/components/pages/MetodologiaOso/MetodologiaOso.jsx
+++ src/components/pages/MetodologiaOso/MetodologiaOso.jsx
@@ -1,12 +1,29 @@
 import React from 'react'
 import Header from '../../Header/Header'
+import Tabs from '@mui/material/Tabs';
+import Tab from '@mui/material/Tab';
+import Box from '@mui/material/Box';
 
-export const MetodologiaOso = () => {
+const [value, setValue] = React.useState('one');
+
+export const MetodologiaOso = (event, newValue) => {
+  setValue(newValue);
+};  
   return (
     <div>
-     <Header></Header>
+      <Header></Header>
+      <Box sx={{width: '100%'}}>
+        <Tabs
+          value={value}
+          onChange={handleChange}
+          textColor='primary'
+          indicatorColor='secondary'
+          aria-label='secontady tabs example'
+          >
+            <Tab value="one" label="Item One"></Tab>
+          </Tabs>
+      </Box>
       oso
     </div>
   )
-}
 export default MetodologiaOso;
