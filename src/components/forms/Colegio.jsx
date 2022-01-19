e965415c50f432d829b6e88d40f23bc47c9aa5c2
Danna <thedannitazz@gmail.com>
2022-01-18 22:14:01 -0500

    Se maqueta el formulario para colegio

---
+++ src/components/forms/Colegio.jsx
@@ -0,0 +1,49 @@
+import React from "react";
+import Header from '../Header/Header';
+import TextField from '@mui/material/TextField';
+import Button from '@mui/material/Button';
+import Card from '@mui/material/Card';
+import './Colegio.css'
+
+export const SchoolForm = () => {
+
+
+
+    return (
+        <div>
+            <Header xs={12}></Header>
+
+            <div className='formContainer'>
+                <Card className="paperForm" elevation={3} >
+
+                <div className="firstField">
+                    <TextField id="standard-basic" label="Nombre" variant="standard"  />
+                </div>
+                <br></br>
+                <div>
+                    <TextField id="standard-basic" label="Ciudad" variant="standard" />
+                </div>
+                <br></br>
+                <div>
+                    <TextField id="standard-basic" label="Departamento" variant="standard" />
+                </div>
+                <br></br>
+                <div>
+                    <TextField id="standard-basic" label="Correo electrónico" variant="standard"  />
+                </div>
+                <br></br>
+                <div>
+                    <TextField id="standard-basic" label="Teléfono" variant="standard"/>
+                </div>
+                <br></br>
+                <div>
+                <Button variant="contained" xs={12} md={12} className="btn-send" >Enviar</Button>
+                </div>
+                </Card>
+                
+            </div>
+        </div>
+    )
+}
+
+export default SchoolForm
