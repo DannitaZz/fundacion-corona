a6a2d242997f8532460b921149a8c2632fbda70c
TefyRabih <Tefyrabih@gmail.com>
2022-01-20 01:37:23 -0500

    Documentos para imprimir ajustes link documentos en general

--- src/components/pages/DocGestion/DocGestion.jsx
+++ src/components/pages/DocGestion/DocGestion.jsx
@@ -19,8 +19,8 @@ const DocMetodologia = () => {
           <AccordionDetails>
             <Grid container className="ContainerCard">
             <div className="menu-item-container">
-            <DocumentCard img="images/letter.png" title="Carta de compromisos IE-Rector" link=''></DocumentCard>
-            <DocumentCard img="images/letter.png" title="Carta consentimiento informado_IE_Rector" link=''></DocumentCard>
+            <DocumentCard img="images/letter.png" title="Carta de compromisos IE-Rector" link='https://firebasestorage.googleapis.com/v0/b/f-corona-oso.appspot.com/o/documentos%20de%20gestion%2FCarta%20de%20compromisos_IE_Rector.pdf?alt=media&token=4485df36-8d7e-463b-932b-3a0031f212d1'></DocumentCard>
+            <DocumentCard img="images/letter.png" title="Carta consentimiento informado_IE_Rector" link='https://firebasestorage.googleapis.com/v0/b/f-corona-oso.appspot.com/o/documentos%20de%20gestion%2FCarta%20consentimiento%20informado_IE_Rector.pdf?alt=media&token=0c0c0ec0-d6f0-4edc-b6b0-7b5947ee0555'></DocumentCard>
             <DocumentCard img="images/encuesta.png" title="Encuesta Rectores" link=''></DocumentCard>
             </div> 
             </Grid>
@@ -33,15 +33,15 @@ const DocMetodologia = () => {
           <AccordionDetails>
             <Grid container className="ContainerCard">
               <div className="menu-item-container">
-              <DocumentCard img="images/segRectores.png" title="Seguimiento Rectores" link=''></DocumentCard>
-            <DocumentCard img="images/lbase.png" title="Línea Base y Seguimiento" link=''></DocumentCard>
-            <DocumentCard img="images/lsalida.png" title="Línea de Salida" link=''></DocumentCard>
-            <DocumentCard img="images/logros.png" title="Formato Metas y alcances" link=''></DocumentCard>
-            <DocumentCard img="images/seguimiento.png" title="Formato Seguimiento" link=''></DocumentCard>
-            <DocumentCard img="images/guia.png" title="2_Guías Noveno" link=''></DocumentCard>
-            <DocumentCard img="images/guiaD.png" title="3_Guías Décimo" link=''></DocumentCard>
-            <DocumentCard img="images/guiaO.png" title="4_Guías Once" link=''></DocumentCard>
-            <DocumentCard img="images/herramientas.png" title="correo Seguimiento periódico" link=''></DocumentCard>
+              <DocumentCard img="images/segRectores.png" title="Seguimiento Rectores" link='https://firebasestorage.googleapis.com/v0/b/f-corona-oso.appspot.com/o/instrumentos%20de%20medicion%2FInstrumento%20de%20seguimiento%20Rectores.pdf?alt=media&token=270b8f08-9f2b-4499-83d4-30bcd7a8fc43'></DocumentCard>
+            <DocumentCard img="images/lbase.png" title="Línea Base y Seguimiento" link='https://firebasestorage.googleapis.com/v0/b/f-corona-oso.appspot.com/o/instrumentos%20de%20medicion%2FInstrumento%20L%C3%ADnea%20Base%20y%20Seguimiento.pdf?alt=media&token=566f8383-eeff-4104-9d6f-0b4fdada15ff'></DocumentCard>
+            <DocumentCard img="images/lsalida.png" title="Línea de Salida" link='https://firebasestorage.googleapis.com/v0/b/f-corona-oso.appspot.com/o/instrumentos%20de%20medicion%2FInstrumento%20L%C3%ADnea%20Salida.pdf?alt=media&token=92ace810-984b-44c0-aafa-922247be0c92'></DocumentCard>
+            <DocumentCard img="images/logros.png" title="Formato Metas y alcances" link='https://firebasestorage.googleapis.com/v0/b/f-corona-oso.appspot.com/o/documentos%20de%20gestion%2FFormato%20de%20metas%20y%20alcances%20aliados%20OSO.xlsx?alt=media&token=65b256f7-2375-4c80-ac3c-3e91c2198967'></DocumentCard>
+            <DocumentCard img="images/seguimiento.png" title="Formato Seguimiento" link='https://firebasestorage.googleapis.com/v0/b/f-corona-oso.appspot.com/o/documentos%20de%20gestion%2FFormato%20seguimiento.xlsx?alt=media&token=d0b6b82d-1c94-4a5f-9fac-3ba225fe6695'></DocumentCard>
+            <DocumentCard img="images/guia.png" title="2_Guías Noveno" link='https://firebasestorage.googleapis.com/v0/b/f-corona-oso.appspot.com/o/documentos%20de%20implementacion%2F2_GUIAS_NOVENO.pdf?alt=media&token=a20a3ad5-b03e-4d67-93bc-af54ef55c614'></DocumentCard>
+            <DocumentCard img="images/guiaD.png" title="3_Guías Décimo" link='https://firebasestorage.googleapis.com/v0/b/f-corona-oso.appspot.com/o/documentos%20de%20implementacion%2F3_GUIAS_DECIMO.pdf?alt=media&token=6a947b43-047f-47f1-b5c8-052997d82d02'></DocumentCard>
+            <DocumentCard img="images/guiaO.png" title="4_Guías Once" link='https://firebasestorage.googleapis.com/v0/b/f-corona-oso.appspot.com/o/documentos%20de%20implementacion%2F4_GUIAS_ONCE.pdf?alt=media&token=1691bc39-992e-4741-90fb-f3386eb97547'></DocumentCard>
+            <DocumentCard img="images/seguimiento.png" title="correo Seguimiento periódico" link='https://firebasestorage.googleapis.com/v0/b/f-corona-oso.appspot.com/o/documentos%20de%20gestion%2Fcorreo%20tipo%20-%20seguimiento%20periodico.docx?alt=media&token=d505564e-28ea-4d60-8083-b7ddceed2a09'></DocumentCard>
               </div>
             </Grid>
           </AccordionDetails>
--- src/components/pages/MatCartillas/MatCartillas.jsx
+++ src/components/pages/MatCartillas/MatCartillas.jsx
@@ -1,20 +1,20 @@
 import React from "react";
-import "../DocGestion/DocGestion.css";
+import "./MatCartillas.css";
 import { Grid } from "@mui/material";
 import DocumentCard from '../../DocumentCard/DocumentCard'
 
 const MatCartillas = () => {
   return (
     <div>
-      <Grid container className="ContainerCard">
-        <div className="menu-item-container">
+      <Grid container className="containerMat">
+        <div className="menu-container-mat">
           <DocumentCard
-            img="images/portada.png"
+            img="images/folderPortada.png"
             title="FOLDER DE PORTADAS"
             link="https://firebasestorage.googleapis.com/v0/b/f-corona-oso.appspot.com/o/Insumos%20para%20impresion%2F0%20_FOLDER_PORTADAS_TODO.pdf?alt=media&token=3ec189a2-f5b6-4ea9-9ae3-909e92f90ba4"
           ></DocumentCard>
           <DocumentCard
-            img="images/guia.png"
+            img="images/lbase.png"
             title="Portada Marco General"
             link="https://firebasestorage.googleapis.com/v0/b/f-corona-oso.appspot.com/o/Insumos%20para%20impresion%2F1%20_%20PORTADA%20MARCO%20GENERAL.pdf?alt=media&token=1b577e9d-d1f8-4f52-9b6d-2ea005d65a38"
           ></DocumentCard>
@@ -24,7 +24,7 @@ const MatCartillas = () => {
             link="https://firebasestorage.googleapis.com/v0/b/f-corona-oso.appspot.com/o/Insumos%20para%20impresion%2F2%20_%20PORTADA%20NOVENO.pdf?alt=media&token=0f837137-ad1a-489b-ace8-5bda5ce36b31"
           ></DocumentCard>
            <DocumentCard
-            img="images/encuesta.png"
+            img="images/folderPortada.png"
             title="Separadores Noveno"
             link="https://firebasestorage.googleapis.com/v0/b/f-corona-oso.appspot.com/o/Insumos%20para%20impresion%2F2.1%20_%20SEPARADORES_NOVENO.pdf?alt=media&token=6184698f-070d-46ac-8aa2-6a3414428ffa"
           ></DocumentCard>
@@ -34,12 +34,12 @@ const MatCartillas = () => {
             link="https://firebasestorage.googleapis.com/v0/b/f-corona-oso.appspot.com/o/Insumos%20para%20impresion%2F3%20_%20PORTADA%20DECIMO.pdf?alt=media&token=3b14f4fe-0c01-4fd2-a203-c5490320ec7d"
           ></DocumentCard>
            <DocumentCard
-            img="images/encuesta.png"
+            img="images/separadorD.png"
             title="Separadores Décimo"
             link="https://firebasestorage.googleapis.com/v0/b/f-corona-oso.appspot.com/o/Insumos%20para%20impresion%2F3.1%20_%20SEPARADOR%20DECIMO.pdf?alt=media&token=73d46513-78aa-452e-bdc8-31b319bc190f"
           ></DocumentCard>
            <DocumentCard
-            img="images/encuesta.png"
+            img="images/seguimiento.png"
             title="Portada Once"
             link="https://firebasestorage.googleapis.com/v0/b/f-corona-oso.appspot.com/o/Insumos%20para%20impresion%2F4%20_%20PORTADa%20ONCE.pdf?alt=media&token=b80503ce-92bf-4b99-99d5-4dc2b83169a0"
           ></DocumentCard>
@@ -49,7 +49,7 @@ const MatCartillas = () => {
             link="https://firebasestorage.googleapis.com/v0/b/f-corona-oso.appspot.com/o/Insumos%20para%20impresion%2F4_1%20SEPARADOR%20ONCE.pdf?alt=media&token=0b1c9978-a2c1-4921-a63f-346b94682e3d"
           ></DocumentCard>
           <DocumentCard
-            img="images/encuesta.png"
+            img="images/portada.png"
             title="Portada y Contraportada"
             link="https://firebasestorage.googleapis.com/v0/b/f-corona-oso.appspot.com/o/Insumos%20para%20impresion%2F5%20_%20PORTADA%20Y%20CONTRAPORTADA.pdf?alt=media&token=c1222815-87d0-4427-9eba-36539dbc9d19"
           ></DocumentCard>
