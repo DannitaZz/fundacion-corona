a929b92a587bfd3d7d96249a03617b3207a65cb0
Danna <thedannitazz@gmail.com>
2022-01-20 12:41:55 -0500

    Se linkea el consolidado de resultados

--- src/components/pages/Aliado/Aliado.jsx
+++ src/components/pages/Aliado/Aliado.jsx
@@ -6,17 +6,17 @@ import AliadosMenuItem from '../../AliadosMenuItem/AliadosMenuItem'
 const Aliado = () => {
   return (
     <div className="Aliado-container">
-      
+
       <main>
         <div className="menu">
-          
+
           <div className="menu-item-container">
             <AliadosMenuItem
               link="BancoHerramientas"
               img="images/herramientas.png"
               text="Banco de herramientas"
               tooltip="Conoce los diferentes documentos de apoyo en torno a Orientación Socio Ocupacional">
-              </AliadosMenuItem>
+            </AliadosMenuItem>
           </div>
           <div className="menu-item-container">
             <AliadosMenuItem
@@ -24,7 +24,7 @@ const Aliado = () => {
               img="images/metologia.png"
               text="Información Metodologia oso"
               tooltip="Encuentra todos los documentos e información para el desarrollo del Programa">
-              </AliadosMenuItem>
+            </AliadosMenuItem>
           </div>
           <div className="menu-item-container">
             <AliadosMenuItem
@@ -32,22 +32,23 @@ const Aliado = () => {
               img="images/school.png"
               text="Información de colegio"
               tooltip="Administra aquí la información de las Instituciones Educativas en las que implementas el programa de OSO">
-              </AliadosMenuItem>
+            </AliadosMenuItem>
           </div>
           <div className="menu-item-container">
-            <AliadosMenuItem
-              link="ConsolidadoResul"
-              img="images/resultados.png"
-              text="Consolidado de resultados"
-              tooltip="Tablero con toda la información de las implementaciones realizadas">
-              </AliadosMenuItem>
+            <div className="menu-item tooltip">
+              <div className="menuItem-container">
+                <img src="images/resultados.png" alt="actualizar" />
+                <a target="blank" href="https://app.powerbi.com/view?r=eyJrIjoiY2EyOTAzYWEtYzEyOS00ODc5LTk0OWMtMWQwMzQyMmRlYTNkIiwidCI6ImZjNTMxMjM3LTQzYzYtNDRkZS1hMWU4LWQzYmY2ZGM0MzZlMSIsImMiOjR9">Consolidado de resultados</a>
+              </div>
+              <span class="tooltiptext">Tablero con toda la información de las implementaciones realizadas</span>
+            </div>
           </div>
           <div className="menu-item-container">
             <AliadosMenuItem
               link="ActualizacionInfo"
               img="images/actualizar.png"
               text="Actualizar información">
-              </AliadosMenuItem>
+            </AliadosMenuItem>
           </div>
         </div>
       </main>
