c1685b9c8f936b2051dbb283d3e6d710966f98d3
natafajado <nataliafajardor1207@gmail.com>
2022-01-20 16:22:30 +0100

    creacion de tooltip para alidos y correcrion estilos en banco gerramientas

--- src/components/AliadosMenuItem/AliadosMenuItem.jsx
+++ src/components/AliadosMenuItem/AliadosMenuItem.jsx
@@ -2,15 +2,16 @@ import React from 'react';
 import { Link } from "react-router-dom";
 import "./AliadosMenuItem.css";
 
-const AliadosMenuItem = ({link, img, text}) => {
+const AliadosMenuItem = ({link, img, text, tooltip}) => {
   return (
-    <div className="menu-item">
+    <div className="menu-item tooltip">
       <Link to={`/${link}`}>
         <div className="menuItem-container">
           <img src={`./${img}`} alt="actualizar" />
           <span>{text}</span>
         </div>
       </Link>
+      {tooltip ? <span class="tooltiptext">{tooltip}</span> : ''}
     </div>
   )
 }
--- src/components/pages/Aliado/Aliado.jsx
+++ src/components/pages/Aliado/Aliado.jsx
@@ -11,19 +11,43 @@ const Aliado = () => {
         <div className="menu">
           
           <div className="menu-item-container">
-            <AliadosMenuItem link="BancoHerramientas" img="images/herramientas.png" text="Banco de herramientas"></AliadosMenuItem>
+            <AliadosMenuItem
+              link="BancoHerramientas"
+              img="images/herramientas.png"
+              text="Banco de herramientas"
+              tooltip="Conoce los diferentes documentos de apoyo en torno a Orientación Socio Ocupacional">
+              </AliadosMenuItem>
           </div>
           <div className="menu-item-container">
-            <AliadosMenuItem link="MetodologiaOso" img="images/metologia.png" text="Información Metodologia oso"></AliadosMenuItem>
+            <AliadosMenuItem
+              link="MetodologiaOso"
+              img="images/metologia.png"
+              text="Información Metodologia oso"
+              tooltip="Encuentra todos los documentos e información para el desarrollo del Programa">
+              </AliadosMenuItem>
           </div>
           <div className="menu-item-container">
-            <AliadosMenuItem link="ConsultarColegio" img="images/school.png" text="Información de colegio"></AliadosMenuItem>
+            <AliadosMenuItem
+              link="ConsultarColegio"
+              img="images/school.png"
+              text="Información de colegio"
+              tooltip="Administra aquí la información de las Instituciones Educativas en las que implementas el programa de OSO">
+              </AliadosMenuItem>
           </div>
           <div className="menu-item-container">
-            <AliadosMenuItem link="ConsolidadoResul" img="images/resultados.png" text="Consolidado de resultados"></AliadosMenuItem>
+            <AliadosMenuItem
+              link="ConsolidadoResul"
+              img="images/resultados.png"
+              text="Consolidado de resultados"
+              tooltip="Tablero con toda la información de las implementaciones realizadas">
+              </AliadosMenuItem>
           </div>
           <div className="menu-item-container">
-            <AliadosMenuItem link="ActualizacionInfo" img="images/actualizar.png" text="Actualizar información"></AliadosMenuItem>
+            <AliadosMenuItem
+              link="ActualizacionInfo"
+              img="images/actualizar.png"
+              text="Actualizar información">
+              </AliadosMenuItem>
           </div>
         </div>
       </main>
