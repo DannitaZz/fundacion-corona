bcfad2e9ceb8545614982591bfea8c2e8f95f118
catarogu08 <cataguatame2963@gmail.com>
2022-01-20 01:00:09 -0500

    icono separador

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -1,5 +1,6 @@
 import React from "react";
 import './Footer.css'
+import footerSch from '../../images/imgFooter.svg';
 
 
 const Footer = () => {
@@ -8,7 +9,7 @@ const Footer = () => {
     <div className="body">
       <div className="footer">
         <h3 className="inforTitle">Fundación Corona  2019 - Todos los derechos reservados</h3>
-        <img src="" alt="" sizes="" srcset="" />
+        <img className="footerImg" src={footerSch} alt=""/>
       </div>
       </div>
     </>
